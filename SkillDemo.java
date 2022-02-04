c5586a5f67bc7daab106f7b3c05cd7eb2fc01441
Goldar Luu <gluu41@gmail.com>
2022-02-04 02:33:55 -0800

    Added test libraries and failing test case

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,2 +1,7 @@
-public class SkillDemo.java {} 
+public class SkillDemo {
+    public static int add(int num1, int num2) {
+        return num1 + num2; 
+    }
+} 
+
 // lets do the rest in vscode bc its faster! 
